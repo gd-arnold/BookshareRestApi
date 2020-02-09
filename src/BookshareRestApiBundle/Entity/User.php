42eee0cb2036dc14dc4d40f3d8f38e8e22a1ddb9
GeorgiArn <georgearnaudov1@gmail.com>
2020-02-09 22:50:37 +0200

    Create user entity

--- src/BookshareRestApiBundle/Entity/User.php
+++ src/BookshareRestApiBundle/Entity/User.php
@@ -1,21 +1,51 @@
 <?php
-// src/BookshareRestApiBundle/Entity/User.php
 
 namespace BookshareRestApiBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\AttributeOverride;
+use Doctrine\ORM\Mapping\AttributeOverrides;
+use Doctrine\ORM\Mapping\Column;
 
 /**
  * User
  *
- * @ORM\Table("users")
- * @ORM\Entity
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="BookshareRestApiBundle\Repository\UserRepository")
+ *
+ *
+ * @AttributeOverrides({
+ *      @AttributeOverride(name="username",
+ *          column=@Column(
+ *              name     = "username",
+ *              nullable = false,
+ *              unique   = true,
+ *              length   = 255
+ *          )
+ *      ),
+ *      @AttributeOverride(name="email",
+ *          column=@Column(
+ *              name     = "email",
+ *              nullable = false,
+ *              unique   = true,
+ *              length   = 255
+ *          )
+ *      ),
+ *      @AttributeOverride(name="password",
+ *          column=@Column(
+ *              name     = "password",
+ *              nullable = false,
+ *              unique   = false,
+ *              length   = 255
+ *          )
+ *      )
+ * })
  */
 class User extends BaseUser
 {
     /**
-     * @var integer
+     * @var int
      *
      * @ORM\Column(name="id", type="integer")
      * @ORM\Id
@@ -23,14 +53,166 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstName", type="string", length=255)
+     */
+    protected $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lastName", type="string", length=255)
+     */
+    protected $lastName;
+
 
     /**
      * Get id
      *
-     * @return integer
+     * @return int
      */
     public function getId()
     {
         return $this->id;
     }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $email = is_null($email) ? '' : $email;
+        parent::setEmail($email);
+        $this->setUsername($email);
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set firstName
+     *
+     * @param string $firstName
+     *
+     * @return User
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * Get firstName
+     *
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * Set lastName
+     *
+     * @param string $lastName
+     *
+     * @return User
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * Get lastName
+     *
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
 }
+
