365e7bc1bbaea511192b509d874f129bd71b95fc
GeorgiArn <georgearnaudov1@gmail.com>
2020-02-09 23:27:11 +0200

    Create encryption service

---
+++ src/BookshareRestApiBundle/Service/Encryption/BCryptService.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace BookshareRestApiBundle\Service\Encryption;
+
+
+use BookshareRestApiBundle\Service\Encryption\EncryptionServiceInterface;
+
+class BCryptService implements EncryptionServiceInterface
+{
+
+    public function hash(string $password)
+    {
+        return password_hash($password, PASSWORD_BCRYPT);
+    }
+
+    public function verify(string $password, string $hash)
+    {
+        return password_verify($password, $hash);
+    }
+}
